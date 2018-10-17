2ba2a5552e561a6d881f22cd73ad9168506c4b04
ardhapanduwinata <ardhapanduwinata@gmail.com>
2018-10-17 22:46:57 +0700

    sumpah ini ganti nama folder kehapus kah semuanya? :(

---
+++ netizen/ardhapanduwinata/BelajarHitung.java
@@ -0,0 +1,9 @@
+public class BelajarHitung{
+	public static void main(String args[])
+	{
+		int angka1 = 5;
+		int angka2 = 3;
+		int hasil = angka1+angka2;
+		System.out.println("Penambahan angka "+angka1+" dan angka "+angka2+" adalah "+hasil);
+	}
+}
