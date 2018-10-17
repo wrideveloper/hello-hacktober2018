93b2ec35b2ad255d2bf1d5a461f169c4dccae691
ardhapanduwinata <ardhapanduwinata@gmail.com>
2018-10-17 22:58:10 +0700

    ini kalo pake Scanner menerima inputan

--- netizen/ardhapanduwinata/BelajarHitung.java
+++ netizen/ardhapanduwinata/BelajarHitung.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
 public class BelajarHitung{
 	public static void main(String args[])
 	{
-		int angka1 = 5;
-		int angka2 = 3;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Masukkan angka1: ");
+		int angka1 = sc.nextInt();
+		System.out.print("Masukkan angka2: ");
+		int angka2 = sc.nextInt();
 		int hasil = angka1+angka2;
-		System.out.println("Penambahan angka "+angka1+" dan angka "+angka2+" adalah "+hasil);
+		System.out.println("\n\nPenambahan angka "+angka1+" dan angka "+angka2+" adalah "+hasil);
 	}
 }
